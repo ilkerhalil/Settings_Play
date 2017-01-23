01f297167976af405611abe906a29a1efb140079
Ilker Halil Türer <iturer@hurriyetemlak.com>
2017-01-23 17:05:33 +0300

    first commit

---
+++ Settings_Play/ConfigurationSections/RedisConnectionSection.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace Settings_Play.ConfigurationSections {
+    public class RedisConnectionSection : ConfigurationSection {
+
+        [ConfigurationProperty("Host")]
+
+        public string Host {
+            get { return this["Host"] as string; }
+            set { this["Host"] = value; }
+        }
+        [ConfigurationProperty("Password")]
+
+        public string Password {
+            get { return this["Password"] as string; }
+            set { this["Password"] = value; }
+        }
+
+        [ConfigurationProperty("Port", DefaultValue = 6379)]
+
+        public int Port {
+            get { return (int)this["Port"]; }
+            set { this["Port"] = value; }
+        }
+    }
+}
